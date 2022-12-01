f95633592075c667277ca086faeea347b0aa96f2
Bernie Petters <bp4924@gmail.com>
2022-12-01 18:08:39 -0500

    link titles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,15 +2,40 @@ import React from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+  /* replace li's using map */
+  const navLinks = [
+    {
+      id: 1,
+      link: "About Me",
+    },
+    {
+      id: 2,
+      link: "Projects",
+    },
+    {
+      id: 3,
+      link: "resume",
+    },
+    {
+      id: 4,
+      link: "Contact",
+    },
+  ];
+
   return (
-    <div className="flex justify-between items-center w-full h-20 p-8 text-white bg-blue-900 capitalize">
+    <div className="flex justify-between items-center w-full h-auto p-8 text-white bg-blue-900 fixed capitalize md:flex-row">
       <div>
         <h1 className="text-4xl font-myName">Bernie Petters</h1>
       </div>
-      <ul>
-        <li className="cursor-pointer font-medium text-gray-300 hover:scale-110">
-          home
-        </li>
+      <ul className="hidden md:flex">
+        {navLinks.map(({ id, link }) => (
+          <li
+            key={id}
+            className="cursor-pointer font-medium text-gray-300 w-auto p-4 hover:scale-110"
+          >
+            {link}
+          </li>
+        ))}
       </ul>
     </div>
   );
