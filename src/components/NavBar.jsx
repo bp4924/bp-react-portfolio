d2141f848d0ce3fe23f515bce0d563a32d30abb6
Bernie Petters <bp4924@gmail.com>
2022-12-04 07:57:29 -0500

    hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
-  /* replace li's using map */
+  const [nav, setNav] = useState(false);
+
   const navLinks = [
     {
       id: 1,
@@ -23,7 +24,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-auto p-8 text-white bg-blue-900 fixed capitalize md:flex-row">
+    <div className="flex justify-between items-center w-full h-20 p-8 text-white bg-blue-900 fixed capitalize md:flex-row">
       <div>
         <h1 className="text-4xl font-myName">Bernie Petters</h1>
       </div>
@@ -37,6 +38,25 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer text-gray-300 w-auto p-4 hover:scale-110 md:hidden"
+      >
+        {nav ? <FaTimes size={20} /> : <FaBars size={20} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col items-center absolute top-24 left-0 w-full h-auto bg-blue-900">
+          {navLinks.map(({ id, link }) => (
+            <li
+              key={id}
+              className="cursor-pointer font-medium text-gray-300 w-auto p-4 hover:scale-110"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
