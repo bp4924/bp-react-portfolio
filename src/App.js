b482f6bdd4d1837ed401e6eb9d0d00b1c978f2d1
Bernie Petters <bp4924@gmail.com>
2022-12-06 09:13:41 -0500

    project screenshots

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Projects />
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div name="projects" className="h-screen w-full">
+      <div className="flex flex-col justify-center p-4 m-auto max-w-screen-lg">
+        <div className="text-2xl font-bold text-blue-900 bg-orange-500 p-4 rounded-lg mb-4 text-center m-0">
+          <p>Projects</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <button>demo</button>
+            <button>code</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
