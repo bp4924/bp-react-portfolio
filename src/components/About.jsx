094418ee832464fe8fd22fe1e9f7c98e974d1c9a
Bernie Petters <bp4924@gmail.com>
2022-12-06 07:48:30 -0500

    component files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div name="about" className="h-screen w-full">
+      <div className="flex flex-col justify-center p-4 m-auto max-w-screen-lg">
+        <div className="text-2xl font-bold text-blue-900 bg-orange-500 p-4 rounded-lg mb-4 text-center m-0">
+          <p>About</p>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+          numquam dolores sequi repellat vero odio hic saepe et, blanditiis
+          molestiae eum a iure labore, adipisci, corporis laudantium ut ipsa
+          aliquid.
+        </p>
+
+        <br />
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor amet
+          recusandae ducimus perspiciatis possimus explicabo, architecto animi
+          eos. Mollitia, tempora veniam! Laudantium quis modi esse!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,22 @@
 // using rafce snippet
 import React from "react";
 // initial screen image from istockphoto.com
-//import splashScreen from "../assets/vertical-code.png";
+import splashScreen from "../assets/vertical-code.png";
 
 const Home = () => {
   return (
     <div
       name="home"
       className="h-full w-full top-20"
-      /*       ADD background image without covering up drop-down links
+      /*       ADD background image without covering up drop-down links       */
       style={{ backgroundImage: `url(${splashScreen})` }}
-       */
     >
-      <div className="flex flex-col items-center justify-center h-screen max-w-screen-lg m-auto mx-4">
-        <div className="flex flex-col justify-center">
-          <h3 className="text-4xl font-bold text-blue-900  bg-orange-500 p-4 rounded-lg mb-4 text-center ">
+      <div className="flex flex-col items-center justify-center h-screen max-w-screen-lg m-auto">
+        <div className="flex flex-col justify-center items-center">
+          <h3 className="text-4xl font-bold text-blue-900  bg-orange-500 p-4 rounded-lg mb-4 text-center m-0 ">
             Full Stack Developer
           </h3>
-          <p className="bg-blue-900 italic text-gray-300 text-lg p-4 rounded-lg ">
+          <p className="bg-blue-900 italic text-gray-300 text-lg mx-6 p-4 rounded-lg text-center">
             Pilot | Runner | Navy Vet <br /> Indecision may or may not be my
             biggest problem.
           </p>
