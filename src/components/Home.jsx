ff3343466cc47361b66879e739a02e8d97954926
Bernie Petters <bp4924@gmail.com>
2024-05-10 11:54:28 -0400

    home page quote

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,12 @@ const Home = () => {
     <div name="home" className="h-full w-full">
       <div className="flex flex-col justify-center h-screen max-w-screen-lg m-auto">
         <div className="text-4xl font-bold text-slate-900  bg-orange-500 p-4 rounded-lg mb-4 text-center ">
-          Full Stack Developer | Software Engineer
+          Software Engineer | Full Stack Developer
         </div>
         <div className="bg-slate-900 italic text-gray-300 text-lg w-full p-4 rounded-lg text-center">
           <p>
-            Pilot | Runner | Navy Veteran <br /> Indecision may or may not be my
-            biggest problem.
+            Pilot | Runner | Navy Veteran <br /> I can't change the direction of
+            the wind, but I can adjust my sails to always reach my destination.
           </p>
         </div>
       </div>
